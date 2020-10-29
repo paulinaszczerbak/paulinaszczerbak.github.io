a41f866a6a77425ed56e0c1338b8cc0ac5820487
apple <apple@MacBook-Pro-apple.local>
2020-10-30 00:28:32 +0100

    Fixed navbar, updated 'about' section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,26 @@
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+let navbar = document.getElementById("navbar");
+let topBtn = document.getElementById("home-btn");
 let home = document.getElementById("home");
 
 // Get the offset position of the navbar
 // var sticky = navbar.offsetTop;
 // var sticky = about.offsetTop;
-var sticky = home.offsetHeight;
+let sticky = home.offsetHeight;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
 	navbar.classList.add("sticky");
 	navbar.classList.remove("hidden");
+    topBtn.classList.add("sticky");
+    topBtn.classList.remove("hidden");
   } else {
     navbar.classList.remove("sticky");
     navbar.classList.add("hidden");
+    topBtn.classList.remove("sticky");
+    topBtn.classList.add("hidden");
   }
 }
