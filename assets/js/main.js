138e6bdf427356942d6e7552314b49acbb05eb4f
Paulina <paulinaszczerbak@gmail.com>
2021-01-19 00:33:23 +0100

    Added more image resolutions to support responsiveness. Updated title.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,10 @@
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
-
 // Get the navbar
-let navbar = document.getElementById("navbar");
-let home = document.getElementById("home");
-
+let navbar = document.querySelector("#navbar");
+let home = document.querySelector("#home");
 // Get the offset position of the navbar
 let sticky = home.offsetHeight;
-
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
